ec72e75307e9d90fa9ca59bfda67c5d98cc3d5d8
yutai <yutai51120@gmail.com>
2021-11-03 17:18:10 +0800

    .

--- HW2/part1/pi.cpp
+++ HW2/part1/pi.cpp
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 #include <pthread.h>
 
 using ll = long long int;
@@ -9,14 +8,13 @@ ll global_number_in_circle = 0;
 ll tosses, threadNum;
 
 void *count_pi(void *arg){
-    double x, y;
     int pos = *(int*)arg;
     ll local_numbers_in_circle = 0;
     
     unsigned seed = (int)time(NULL) + pos;    
     for(int i = pos; i < tosses; i += threadNum){
-        x = (double)rand_r(&seed) / RAND_MAX;        
-        y = (double)rand_r(&seed) / RAND_MAX;        
+        double x = (double)rand_r(&seed) / RAND_MAX;        
+        double y = (double)rand_r(&seed) / RAND_MAX;        
         if(x * x + y * y <= 1.0){
             local_numbers_in_circle++;
         }
@@ -34,16 +32,12 @@ int main(int argc, char *argv[]){
     pthread_t thds[threadNum];
     for(int i = 0; i < threadNum; ++i){
         int *ptr = new int(i);
-        if(pthread_create(&thds[i], NULL, count_pi, (void*)ptr)){
-            perror("Failed to create thread!\n");
-        }
+        pthread_create(&thds[i], NULL, count_pi, (void*)ptr);
     }
 
     for(int i = 0; i < threadNum; ++i){
         ll *res;
-        if(pthread_join(thds[i], (void**)&res)){
-            perror("Failed to join thread!\n");
-        }
+        pthread_join(thds[i], (void**)&res);
         global_number_in_circle += *res;
         free(res);
     }
